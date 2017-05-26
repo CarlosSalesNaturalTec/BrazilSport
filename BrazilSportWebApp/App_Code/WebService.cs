7536a7f6c4e1e2297d6b3d652e743890fc76b357
Carlos Sales <carlossalesti@gmail.com>
2017-05-26 12:08:16 -0300

    Login OK

---
+++ BrazilSportWebApp/App_Code/WebService.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string Identificador(string user, string pwd)
+    {
+        string Identificador_msg = "0";
+
+        // localiza usuario
+        string stringSelect = "select senha,nome from tbl_usuarios where usuario = '" + user + "'";
+        OperacaoBanco Identificador_Operacao = new OperacaoBanco();
+        System.Data.SqlClient.SqlDataReader Identificador_rcrdset = Identificador_Operacao.Select(stringSelect);
+        while (Identificador_rcrdset.Read())
+        {
+            if (pwd == Convert.ToString(Identificador_rcrdset[0]))
+            {
+
+                string vValida1, vValida2;
+                vValida1 = DateTime.Now.ToString("dd");
+                vValida2 = DateTime.Now.ToString("MM");
+                int vValida3 = Convert.ToInt16(vValida1) * Convert.ToInt16(vValida2);
+                string vValida4 = vValida3.ToString();
+
+                Identificador_msg = "Index.aspx?p1=" + vValida4 + "&p2=" + Convert.ToString(Identificador_rcrdset[1]);
+
+            }
+            else
+            {
+                Identificador_msg = "2";
+            }
+        }
+        ConexaoBancoSQL.fecharConexao();
+
+        return Identificador_msg;
+
+    }
+
+}
+
+public class ConexaoBancoSQL
+{
+    private static SqlConnection objConexao = null;
+    private string stringconnection1;
+
+    public void tentarAbrirConexaoRemota()
+    {
+        objConexao = new SqlConnection();
+        objConexao.ConnectionString = stringconnection1;
+        objConexao.Open();
+    }
+
+    public ConexaoBancoSQL()
+    {
+        // *** STRING DE CONEXÃO COM BANCO DE DADOS - Atenção! Alterar dados conforme seu servidor
+        stringconnection1 = "Server=tcp:serverbrazilsport.database.windows.net,1433;Initial Catalog=dbbrazilsport;Persist Security Info=False;User ID=admserver;Password=pwd@1973;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        try
+        {
+            tentarAbrirConexaoRemota();
+        }
+        catch
+        {
+            Console.WriteLine("Atenção! Não foi possível Conectar ao Servidor de Banco de Dados.");
+        }
+    }
+
+    public static SqlConnection getConexao()
+    {
+        new ConexaoBancoSQL();
+        return objConexao;
+    }
+    public static void fecharConexao()
+    {
+        objConexao.Close();
+    }
+}
+
+public class OperacaoBanco
+{
+    private SqlCommand TemplateMethod(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return Commando;
+        }
+        catch
+        {
+            return Commando;
+        }
+    }
+
+    public SqlDataReader Select(String query)
+    {
+        SqlDataReader dadosObtidos = TemplateMethod(query).ExecuteReader();
+        return dadosObtidos;
+    }
+
+    public Boolean Insert(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public Boolean Update(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public Boolean Delete(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+}
--- BrazilSportWebApp/Default.aspx
+++ BrazilSportWebApp/Default.aspx
@@ -17,7 +17,7 @@
         <img src="images/brazilsport_logo.png" alt="BrazilSport Logomarca" />
     </header>
 
-    <div class="w3-display-middle">
+    <div id="divLogin" class="w3-display-middle">
         <section class="w3-padding w3-center w3-animate-opacity" style="max-width: 300px">
             <h2><strong>Painel de Controle</strong> </h2>
             <hr />
@@ -29,15 +29,31 @@
                 <input id="inputPwd" placeholder="Senha" class="w3-input" type="password" />
             </p>
             <p>
-                <button id="btEntrar" class="w3-btn w3-green w3-round" >Entrar</button>
+                <button id="btLogin" class="w3-btn w3-green w3-round" onclick="TentarLogin()" >Entrar</button>
             </p>
             <hr />
             <h4><i class="fa fa-futbol-o" aria-hidden="true"></i>&nbsp;BrazilSport</h4>
         </section>
     </div>
-    <script>
-        document.getElementById('inputUser').focus();
-    </script>
+
+
+    <div id="divRetornoSenha" class="w3-display-middle" style="display:none">
+        <section class="w3-padding w3-center w3-animate-left" style="max-width: 300px">
+            <h2><strong>Atenção !</strong> </h2>
+            <hr />
+            <p>
+                <span  id="lblMsgRetorno"></span>
+            </p>
+            <p>
+                <button id="btVoltar" class="w3-btn w3-green w3-round" onclick="LoginVoltar()" >Tentar Novamente</button>
+            </p>
+            <hr />
+            <h4><i class="fa fa-futbol-o" aria-hidden="true"></i>&nbsp;BrazilSport</h4>
+        </section>
+    </div>
+
+    <script src="Scripts/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script src="Scripts/codeLogin.js"></script>
 
 </body>
     
